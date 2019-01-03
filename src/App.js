9ff18ee288a461e5cd4438639bf89069e2971173
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-01-04 00:58:31 +0100

    Add json upload and display to App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import TheDropzone from './components/dropzone';
+import read from './utils/reader';
 
 class App extends Component {
+  state = {
+    src: null,
+  };
+
+  onDrop = async files => {
+    try {
+      const src = await read(files[0]);
+
+      console.log(src);
+
+      this.setState({ src });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  renderSrc(src) {
+    return (
+      <pre className="App-src">
+        <code>{JSON.stringify(src, null, 4)}</code>
+      </pre>
+    );
+  }
+
   render() {
+    const { src } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -20,8 +47,12 @@ class App extends Component {
           >
             Learn React
           </a> */}
-          <TheDropzone className="The-dropzone" />
-          <TheDropzone className="The-dropzone" />
+
+          {src ? (
+            this.renderSrc(src)
+          ) : (
+            <TheDropzone className="The-dropzone" onDrop={this.onDrop} />
+          )}
         </header>
       </div>
     );
