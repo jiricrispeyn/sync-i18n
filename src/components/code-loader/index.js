0af1cc6d4174a68b1ad940bf6a41be6b520dfbe1
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-01-05 16:48:47 +0100

    Add code loader component

---
+++ src/components/code-loader/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Code } from 'react-content-loader';
+
+const CodeLoader = () => <Code style={{ width: '100%', height: '100%' }} />;
+
+export default CodeLoader;
