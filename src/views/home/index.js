f36a705be76d6d74513774f5b6b58bc604589a1f
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-01-06 13:43:40 +0100

    Integrate react-waterfall actions into Home view

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import TheDropzone from '../../components/dropzone';
+import { actions } from '../../store';
 import { Container } from './style';
 import read from '../../utils/reader';
 
 export default class Home extends Component {
-  state = {
-    sourceFile: null,
-  };
-
   onDrop = async files => {
     try {
       const sourceFile = await read(files[0]);
 
-      this.setState({ sourceFile }, () => {
-        this.props.navigate('/upload');
-      });
+      actions.setSourceFile(sourceFile);
+      this.props.navigate('/upload');
     } catch (err) {
       console.log(err);
     }
