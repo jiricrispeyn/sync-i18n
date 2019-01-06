c1debe35e847b0f94dfa831f80bba1e6e282e8aa
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-01-06 13:43:54 +0100

    Create basic upload view

--- src/views/upload/index.js
+++ src/views/upload/index.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import { Container } from './style';
+import { actions } from '../../store';
+import TheDropzone from '../../components/dropzone';
+import read from '../../utils/reader';
 
 export default class Upload extends Component {
+  onDrop = async files => {
+    try {
+      const destinationFile = await read(files[0]);
+
+      actions.setDestinationFile(destinationFile);
+      this.props.navigate('/upload');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   render() {
-    return <div>Upload</div>;
+    return (
+      <Container>
+        <div>Upload</div>
+        <div>
+          <TheDropzone onDrop={this.onDrop} />
+        </div>
+      </Container>
+    );
   }
 }
