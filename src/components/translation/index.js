d5b33938a68208061452f16d2b587feadee4c7c4
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-01-07 16:50:33 +0100

    Make sure key is used in translation recursive loop

--- src/components/translation/index.js
+++ src/components/translation/index.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import ScrollArea from 'react-scrollbar';
 
-export default class Translation extends Component {
-  onScroll = value => {
-    this.props.onScroll(value.topPosition);
-  };
+let index = 0;
 
+export default class Translation extends Component {
   renderLevel(data) {
     const keys = Object.keys(data);
 
     return keys.map(key => {
       if (typeof data[key] === 'object') {
         return (
-          <>
+          <React.Fragment key={index++}>
             <h5>{key}</h5>
             <ul>{this.renderLevel(data[key])}</ul>
-          </>
+          </React.Fragment>
         );
       }
 
       return (
-        <li>
+        <li key={index++}>
           {key}: {data[key]}
         </li>
       );
@@ -31,8 +29,8 @@ export default class Translation extends Component {
     const { data } = this.props;
 
     return (
-      <ScrollArea style={{ flex: 1 }} onScroll={this.onScroll}>
-        <ul style={{ flex: 1 }}>{this.renderLevel(data)}</ul>
+      <ScrollArea>
+        <ul>{this.renderLevel(data)}</ul>
       </ScrollArea>
     );
   }
