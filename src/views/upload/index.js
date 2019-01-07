1bd9a15b6fa30eb4cdd96112ccf88ede48fa47bb
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-01-07 16:51:19 +0100

    Create Stripe inspired codeblock for upload view

--- src/views/upload/index.js
+++ src/views/upload/index.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import { Container } from './style';
-import { actions } from '../../store';
+import { Container, CodeBlock } from './style';
+import { connect, actions } from '../../store';
 import TheDropzone from '../../components/dropzone';
 import read from '../../utils/reader';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
 
-export default class Upload extends Component {
+class Upload extends Component {
   onDrop = async files => {
     try {
       const destinationFile = await read(files[0]);
 
       actions.setDestinationFile(destinationFile);
-      this.props.navigate('/upload');
+      this.props.navigate('/edit');
     } catch (err) {
       console.log(err);
     }
   };
 
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
   render() {
     return (
       <Container>
-        <div>Upload</div>
+        <div>
+          <CodeBlock>
+            <header className="CodeBlock-header">
+              <p className="CodeBlock-header-title">Source File</p>
+            </header>
+            <div className="CodeBlock-content">
+              <pre>
+                <code className="language-javascript">
+                  {JSON.stringify(this.props.sourceFile, null, 2)}
+                </code>
+              </pre>
+            </div>
+          </CodeBlock>
+        </div>
         <div>
           <TheDropzone onDrop={this.onDrop} />
         </div>
@@ -27,3 +46,5 @@ export default class Upload extends Component {
     );
   }
 }
+
+export default connect(({ sourceFile }) => ({ sourceFile }))(Upload);
--- src/views/upload/style.js
+++ src/views/upload/style.js
@@ -6,4 +6,61 @@ export const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
+
+  > div {
+    display: flex;
+    width: 50%;
+    height: 100%;
+  }
+`;
+
+export const CodeBlock = styled.div`
+  position: relative;
+  background-color: #fff;
+  margin: 40px;
+  box-shadow: 0 30px 30px 0 rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  transform: rotate3d(0.5, 0.866, 0, 15deg) rotate(-1deg);
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    left: 0;
+    bottom: 0;
+    background-image: linear-gradient(to bottom, rgba(255, 0, 0, 0), #fff);
+  }
+
+  .CodeBlock-header {
+    display: flex;
+    padding: 10px 30px;
+    background-color: #fafafa;
+  }
+
+  .CodeBlock-content {
+    display: flex;
+    padding: 20px 15px;
+  }
+
+  .CodeBlock-header-title {
+    background-color: #2d4ef5;
+    border-radius: 3px;
+    padding: 14px 28px;
+    font-size: 14px;
+    font-weight: bold;
+    color: #fff;
+    text-transform: uppercase;
+    box-shadow: 0 10px 30px 0 rgba(45, 78, 245, 0.5);
+  }
+
+  pre {
+    background-color: #fff !important;
+    margin: 0;
+    white-space: pre-wrap;
+  }
+
+  code {
+    font-size: 10px;
+  }
 `;
